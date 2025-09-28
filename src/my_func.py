76251eaec415bf65d09b32765ed8a39b05215e6d
K hashimoto <keishi04hrikzira@gmail.com>
2025-09-28 18:48:47 +0900

    💪  エラー理由を分かりやすくするために、event のパース処理をデコレータから関数に変更

--- src/my_func.py
+++ src/my_func.py
@@ -2,16 +2,20 @@ from boto3 import client
 
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.data_classes import S3Event
-from aws_lambda_powertools.utilities.parser import event_parser
+from aws_lambda_powertools.utilities.parser import parse, ValidationError
 
 s3_client = client("s3")
 
 
-@event_parser(model=S3Event)
-def my_handler(event: S3Event, context: LambdaContext):
+def my_handler(event: dict, context: LambdaContext):
+    try:
+        parsed_event = parse(event=event, model=S3Event)
+    except ValidationError as e:
+        print(e.json())
+        return
     # S3 イベントの型は以下を参照
     # https://github.com/aws-powertools/powertools-lambda-python/blob/d3d94cc38d72bc99d54bb5864420daa73c8f9ac4/aws_lambda_powertools/utilities/data_classes/s3_event.py#L301
-    s3s = [r.s3 for r in event.records]
+    s3s = [r.s3 for r in parsed_event.records]
     print(s3s)
 
     # TODO: get source file from s3 bucket
